de5585ad6c67fc1cf00b8dc7e4d165bf081f1add
izunya <whdtj5615@naver.com>
2026-07-28 10:51:29 +0000

    memory change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
             exec_mode: 'fork',
             autorestart: true,
             watch: false,
-            max_memory_restart: '500M',
+            max_memory_restart: '1G',
             time: true,
             env: {
                 NODE_ENV: 'production',
